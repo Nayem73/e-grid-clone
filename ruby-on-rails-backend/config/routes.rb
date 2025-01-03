77a740aa01ccbf9e5345321f026c5805ef066a64
nayem73 <nayem73@gmail.com>
2025-01-03 23:47:27 +0600

    add user authentication; implement user model, controller, and JWT token handling

--- ruby-on-rails-backend/config/routes.rb
+++ ruby-on-rails-backend/config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     resources :carousel_translations
     resources :service_softwaredev
     resources :service_softwaredev_results
+    resource :users, only: [:create]
+    post "/login", to: "users#login"
+    get "/auto_login", to: "users#auto_login"
   end
 end
--- ruby-on-rails-backend/db/schema.rb
+++ ruby-on-rails-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_03_044859) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_03_164315) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -98,6 +98,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_03_044859) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_services_on_slug", unique: true
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
